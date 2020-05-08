77aee786e03b0f7693812f2af45b3564101b38d5
Stanislav Bezkorovainyi <stanislavbezkor@gmail.com>
2020-05-08 16:31:44 +0300

    Added loader circle

--- src/index.js
+++ src/index.js
@@ -15,13 +15,15 @@ const clearButton = document.querySelector('.clear-button');
 clearButton.onclick = clearSudokuInput;
 const solveButton = document.querySelector('.solve-button');
 
+const waitingMessage = document.querySelector('.sudoku-waiting-msg');
 solveButton.addEventListener('click', (event) => {
-
   const sudokuSolverWorker = new Worker();
   sudokuSolverWorker.postMessage(getSudokuInput().map(value => parseInt(value)));
+  waitingMessage.style.display = 'block';
 
   sudokuSolverWorker.onmessage = (solverResponse) => {
     setSudokuInput(solverResponse.data.matrix)
     sudokuSolverWorker.terminate();
+    waitingMessage.style.display = 'none';
   };
 });
