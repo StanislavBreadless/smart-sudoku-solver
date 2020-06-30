052f1ec45536b5736a5b20bbaba2a511c7844eb1
Stanislav Bezkorovainyi <stanislavbezkor@gmail.com>
2020-06-30 15:12:20 +0300

    Finished spinning circle

---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const waitingLongTime = 5000;
+export const waitingMessage = 'Please wait until the solver finishes it\'s computations...';
+export const waitingTooLongMessage = 'It takes longer than usual... Are you sure it is a valid sudoku?';
